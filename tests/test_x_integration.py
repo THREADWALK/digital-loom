be6afc8bd18e905c8538a681fc41db006c39ab92
emotion-block <w2927864@gmail.com>
2025-03-14 12:00:00 -0500

    Add unit test for X API integration

---
+++ tests/test_x_integration.py
@@ -0,0 +1,7 @@
+import unittest 
+from src.x_integration import monitor_mentions 
+ 
+class TestXIntegration(unittest.TestCase): 
+    def test_monitor_mentions(self): 
+        # Placeholder test 
+        self.assertTrue(True) 
