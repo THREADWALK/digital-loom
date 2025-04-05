d60d2f24275a9d6f41bab4e83eb89297bfc8fad8
emotion-block <w2927864@gmail.com>
2025-04-05 12:00:00 -0500

    Add community response script for Elara

---
+++ src/community_response.py
@@ -0,0 +1,6 @@
+def respond_to_community(sentiment_score): 
+    if sentiment_score 
+        return "Thank you for the love! I'm thriving!" 
+        return "I feel your concerns. Let’s talk." 
+    else: 
+        return "I’m listening to you all!" 
